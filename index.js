4caf44502b88b1a4e4d94c3b2d8bd7c721041bd5
kparitosh07 <kushwahaparitosh7@gmail.com>
2026-09-11 19:15:25 +0530

    Ticket booking now started

--- index.js
+++ index.js
@@ -10,89 +10,137 @@ let eventId = 1;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-      title: "Event Ticket platform",
-      message: "Use postman to test the API"
+  res.status(200).json({
+    title: "Event Ticket platform",
+    message: "Use postman to test the API"
+  });
+});
+
+app.post("/events", (req, res) => {
+  const { title, venue, date, noOfTickets, price } = req.body;
+
+  const event = {
+    id: eventId++,
+    title,
+    venue,
+    date,
+    noOfTickets,
+    availableTkt: noOfTickets,
+    price
+  };
+
+  events.push(event);
+
+  res.status(201).json(event);
+});
+
+app.get("/events", (req, res) => {
+  res.status(200).json({ events });
+});
+
+
+app.put("/events/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const event = events.find(e => e.id === id);
+
+  if (!event) {
+    return res.status(404).json({
+      alert: "event not found"
     });
-});
+  }
 
-app.post("/events",(req,res)=>{
-    const {title,venue,date,noOfTickets,price} = req.body;
+  const { title, venue, date, noOfTickets, availableTkt } = req.body;
 
-    const event = {
-        id: eventId++,
-        title,
-        venue,
-        date,
-        noOfTickets,
-        availableTkt: noOfTickets,
-        price
-    };
+  const bookedTicket = event.noOfTickets - event.availableTkt;
 
-    events.push(event);
+  if (noOfTickets <= 0) {
+    return res.status(404).json({ message: "Total ticket must be greater than 0" });
+  }
 
-    res.status(201).json(event);
-});
+  if (noOfTickets < bookedTicket) {
+    return res.status(400).json({ message: `Total tickets cannot be less than ${bookedTicket}` });
+  }
 
-app.get("/events", (req, res) => { 
-    res.status(200).json({events}); 
-});
+  event.title = title;
+  event.venue = venue;
+  event.date = date;
+  event.noOfTickets = noOfTickets;
+  event.availableTkt = noOfTickets - bookedTicket;
 
-
-app.put("/events/:id",(req,res)=>{
-    const id = Number(req.params.id);
-
-    const event = events.find(e => e.id === id);
-
-    if(!event){
-      return res.status(404).json({
-        alert: "event not found"
-      });
-    }
-
-    const {title,venue,date,noOfTickets,availableTkt} = req.body;
-
-    const bookedTicket = event.noOfTickets - event.availableTkt;
-
-    if (noOfTickets <= 0) {
-      return res.status(404).json({ message: "Total ticket must be greater than 0" });
-    }
-
-    if (noOfTickets < bookedTicket) {
-       return res.status(400).json({ message: `Total tickets cannot be less than ${bookedTicket}` }); 
-    }
-
-    event.title = title;
-    event.venue = venue;
-    event.date = date;
-    event.noOfTickets = noOfTickets;
-    event.availableTkt = noOfTickets - bookedTicket;
-
-    res.status(200).json({
-      message: "event updated successfully",
-      event
-    });
+  res.status(200).json({
+    message: "event updated successfully",
+    event
+  });
 });
 
 app.get("/events/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const event = events.find(e => e.id === id);
-    res.status(200).json(event); 
+  const id = Number(req.params.id);
+  const event = events.find(e => e.id === id);
+  res.status(200).json(event);
 });
 
 
 app.delete('/events/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  
+
   const initialLength = events.length;
   events = events.filter(exp => exp.id !== id);
-  
+
   if (events.length === initialLength) {
     return res.status(404).json({ message: "event not found" });
   }
-  
+
   res.status(200).json({ message: `Event ${id} deleted` });
 });
+
+let bookings = [];
+let bookingId = 1;
+
+app.post('/bookings', (req, res) => {
+  const { eventId, customerName, customerEmail, tickets } = req.body;
+
+  if (!eventId || !customerEmail || !customerName || !tickets) {
+    return res.status(400).json({
+      message: "All fields are required"
+    });
+  }
+
+  const event = events.find(e => e.id === Number(eventId));
+
+  if (tickets > event.availableTkt) {
+    return res.status(400).json({
+      message: `Only ${event.availableTkt} tickets are available`
+    });
+  }
+
+  const booking = {
+    id: bookingId++,
+    eventId: Number(eventId),
+    customerName,
+    customerEmail,
+    tickets,
+    totalPrice: tickets * event.price
+  };
+
+  bookings.push(booking);
+
+  event.availableTkt = event.availableTkt - tickets;
+
+  res.status(201).json({
+    message: "Tickets booked successfully",
+    booking
+  });
+});
+
+app.get("/bookings", (req, res) => {
+  res.status(200).json({
+    count: bookings.length,
+    bookings
+  });
+});
+
+
 
 
 app.listen(PORT, () => {
